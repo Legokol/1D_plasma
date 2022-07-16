63ff648c82f10185444d12c36e0ec1cb37df6474
legokol <legokol3@gmail.com>
2022-07-17 01:06:21 +0300

    Перенос Constants

---
+++ Constants.hpp
@@ -0,0 +1,10 @@
+//
+// Created by legokol on 16.07.2022.
+//
+
+#ifndef INC_1D_PLASMA_CONSTANTS_HPP
+#define INC_1D_PLASMA_CONSTANTS_HPP
+
+const double epsilon0 =  8.85418781762039e-12; // Электрическая постоянная
+
+#endif //INC_1D_PLASMA_CONSTANTS_HPP
